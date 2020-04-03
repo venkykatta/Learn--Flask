c93288710b3dee72fa73cb81fa56cdf77090e3b9
venkykatta <venkateshkatta9705@gmail.com>
2020-04-03 10:14:50 +0530

    updated

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from urllib import request
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -20,9 +21,20 @@ class Todo(db.Model):
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-       pass
+       task_content = request.form['content']
+       new_task = Todo(content=task_content)
+
+       try:
+          db.session.add(new_task)
+          db.session.commit()
+          return redirect('/')
+
+       except:
+          return "There was an issue adding your task"
+
     else:
-       return render_template('index.html')
+       tasks = Todo.query.order_by(Todo.date_created).all()
+       return render_template('index.html',tasks = tasks)
 
 if __name__ == "__main__":
    app.run(debug=True)
